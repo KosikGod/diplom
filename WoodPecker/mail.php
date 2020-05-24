1988b1735cb4e1f8d64ac8ac6c5c61dc865a8fe0
KosikGod <KosikGod@users.noreply.github.com>
2020-05-24 21:53:00 +0300

    1

---
+++ WoodPecker/mail.php
@@ -0,0 +1,19 @@
+<?php
+$(".form").submit(function() {
+var str = $(this).serialize();
+$.ajax({
+type: "POST",
+url: "mail
+.php",
+data: str,
+success: function(msg) {
+if(msg == 'ok') {
+alert('Письмо отправлено');
+}
+else {
+alert('Ошибка! Возможно вы неправильно заполнили поля.');
+}
+}
+});
+return false;
+});
